932e62a3ca112fdb5dc367f87d3f849a13ddeff3
Sasha <alexsolar@yandex.ru>
2018-08-26 18:15:01 +0300

    Пара компонентов для проверки автосборки после изменений кода

---
+++ src/main/java/io/fabric8/quickstarts/cxf/jaxrs/LoggingRepository.java
@@ -0,0 +1,14 @@
+package io.fabric8.quickstarts.cxf.jaxrs;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LoggingRepository {
+    /**
+     * Сохраняем лог в базу данных
+     * @param message
+     */
+    public void addLog(String message){
+        //TODO сделать реализацию
+    }
+}
---
+++ src/main/java/io/fabric8/quickstarts/cxf/jaxrs/TestService.java
@@ -0,0 +1,10 @@
+package io.fabric8.quickstarts.cxf.jaxrs;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public void loadTestOne(){
+
+    }
+}
